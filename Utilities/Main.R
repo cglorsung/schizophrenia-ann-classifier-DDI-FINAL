161684ad25b9b5ee6c60d2726468fb7e0b5c693f
Conor Lorsung <cglorsung@gmail.com>
2018-04-13 15:40:30 -0500

    Beginning the neural network skeleton

--- Utilities/Main.R
+++ Utilities/Main.R
@@ -1,3 +1,26 @@
-print('Hello, World!');
-value <- 5;
-print(value);
+# Author : Conor Lorsung
+# Purpose: This system is built for my final CSCI-332 project.
+#          Ideally, it will be a neural network that acts as
+#          a classifier for the schizophrenia data outlined
+#          in the github repository this file is hosted in.
+
+# Supervision
+supervise = TRUE
+
+# Number of iterations
+iterations = 10000
+
+# Read data file **TO BE COMPLETED**
+
+# Scale file data
+
+# Put data into matrix datatype
+
+# Sigmoid function
+sigmoid <- function(x, derive=FALSE) {
+    if(derive) {
+        return (x*(1-x))
+    } else {
+        return (1/(1+exp(-x)))
+    }
+}
