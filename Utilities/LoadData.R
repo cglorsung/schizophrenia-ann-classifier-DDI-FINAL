53704725c176288f1d4d3da146bfb2f34a6ac316
Conor Lorsung <cglorsung@gmail.com>
2018-04-30 11:57:41 -0500

    Get subsets

--- Utilities/LoadData.R
+++ Utilities/LoadData.R
@@ -9,8 +9,20 @@ demoDir  <- "../DataFiles/button-tone-sz/"
 demoName <- "demographic"
 demoData <- read.csv(paste(demoDir, demoName, ".csv", sep=""), header<-TRUE)
 
+# Get lists based on class value
 sList <- demoData$subject[(demoData$group == 1)]
 nList <- demoData$subject[(demoData$group == 0)]
+
+# Build dataframe with subject numbers according to class values
+# sList = patients with schizophrenia
+# nList = patients without schizophrenia
+demoFrame <- data.frame(subset(cbind(mVal=
+{
+    n <- max(length(sList), length(nList)) # Max vector length
+    length(sList) <- n                     # Assign length = max length
+    length(nList) <- n                     # ''
+},
+ sList=sList, nList=nList), select=-mVal)) # Dataframe with columns sList & nList
 
 # Read data file
 fileDir  <- "../DataFiles/button-tone-sz/"
